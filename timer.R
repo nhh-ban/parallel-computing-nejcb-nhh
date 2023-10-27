2019d24fdc906d68467b2fd502ac15096553e2fc
nejcb-nhh <nejc.brancelj@student.nhh.no>
2023-10-27 18:20:56 +0200

    Seting up the timer.r file and changing file names

---
+++ timer.R
@@ -0,0 +1,21 @@
+# Calling various libraries
+library(tweedie) 
+library(tidyverse)
+library(tictoc)
+library(doParallel)
+
+
+# Measuring performance of solution 1
+tic()
+source("scripts/original_solution.r")
+toc()
+
+# Measuring performance of solution 2
+tic()
+source("scripts/parallel_solution_1.r")
+toc()
+
+# Measuring performance of solution 3
+tic()
+source("scripts/multicore_solution_2.r")
+toc()
