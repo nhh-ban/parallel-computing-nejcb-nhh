d7e869077b9feb7db704539d1edcb4c5f80d7497
nejcb-nhh <nejc.brancelj@student.nhh.no>
2023-10-27 21:21:26 +0200

    adding comments

--- timer.R
+++ timer.R
@@ -18,3 +18,13 @@ toc()
 tic()
 source("scripts/multicore_solution_2.r")
 toc()
+
+# The fastest method by far is changing the MTweedieTests function
+# to run in parallel (time elapsed: 11.21 s) in comparison to the 
+# original method (time elapsed: 46.91 s) and the method of running
+# the for loop in parallel (time elapsed: 35.66 s). I assume that 
+# the latter did not run much faster since the for loop was moving
+# through a data frame and had to make calculations one by one 
+# regardless. On the other hand changing the MTweedieTests ran
+# much faster due to the function just replicating a task M-amount
+# of times which is considerably faster in parallel execution.
